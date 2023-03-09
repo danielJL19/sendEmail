c4451d8f1cc29ea247b91491bd5c3084cf3002ca
Daniel <danieljmnz15@hotmail.com>
2023-03-09 09:46:39 -0300

    change pass

--- envio.php
+++ envio.php
@@ -23,7 +23,7 @@ if (isset($_POST)) {
         $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'danieljmnz15@hotmail.com';                     //SMTP username
-        $mail->Password   = 'lozano001@';                               //SMTP password
+        $mail->Password   = 'vwlpwgqqwwgjwxep';                               //SMTP password
         $mail->SMTPSecure = 'STARTTLS';            //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
