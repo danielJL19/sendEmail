d25b90f5142c7cc9300ac94d9006d3618be19a4a
Daniel <danieljmnz15@hotmail.com>
2023-03-08 23:43:54 -0300

    primer commit

---
+++ envio.php
@@ -0,0 +1,55 @@
+<?php
+        //Import PHPMailer classes into the global namespace
+        //These must be at the top of your script, not inside a function
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\SMTP;
+        use PHPMailer\PHPMailer\Exception;
+
+if (isset($_POST)) {
+    $nombre=$_POST['nombre'];
+    $correo=$_POST['correo'];
+    $mensaje=$_POST['mensaje'];
+    $asunto=$_POST['asunto'];
+    //Load Composer's autoloader
+    require 'vendor/autoload.php';
+
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = 0; //0 - DESACTIVADO 2-ACTIVADO
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'outlook.office365.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'danieljmnz15@hotmail.com';                     //SMTP username
+        $mail->Password   = 'lozano001@';                               //SMTP password
+        $mail->SMTPSecure = 'STARTTLS';            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('danieljmnz15@hotmail.com', 'Daniel Jiménez');//CORREO QUIEN CREO EL MENSAJE
+        $mail->addAddress($correo, $nombre);     //Add a recipient
+
+    //Optional name
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $asunto;
+        $mail->Body    = $mensaje;
+        
+
+        $mail->send();
+        echo 'Enviado correctamente';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
+
+}
+    
+
+        
+
+        
+?>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enviar Correo</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-xl">
+        <div class="row justify-content-center mt-5">
+
+            <form action="envio.php" method="POST" class="form bg-light p-3">
+                <div class="mb-3">
+                    <label class="form-label" for="nombre">Nombre</label>
+                    <input class="form-control" type="text" name="nombre" id="nombre" placeholder="Daniel">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="correo">Correo</label>
+                    <input class="form-control" type="email" name="correo" id="correo" placeholder="Jiménez">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="asunto">Asunto</label>
+                    <input class="form-control" type="text" name="asunto" id="asunto">
+                </div>
+                <div class="mb-3">
+                    <textarea name="mensaje" id="mensaje" placeholder="Escribe tu mensaje..." cols="30" rows="10"></textarea>
+                </div>
+                <input type="submit" value="Enviar mensaje">
+            </form>
+            
+        </div>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
